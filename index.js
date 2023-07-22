00dd2a1cf06753352847c0762af1cf57d6777af5
lennardevertz <levertz@idriss.xyz>
2023-07-22 22:46:26 +0200

    connect html and js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let connectedAccount;
 let provider;
 let oracle;
 let registry;
+let userId;
+let userHash;
+let userTweet;
 
 
 // load uma oracle contract
@@ -421,7 +424,7 @@ async function init() {
     console.log(connectedAccount);
     await switchNetwork(web3, "goerli", provider);
     document.getElementById("connectWallet").value = "Disconnect";
-    document.getElementById("connectedAddress").innerHTML = "(".concat(connectedAccount.substring(0, 6)).concat("...").concat(connectedAccount.substr(-4)).concat(")");
+    document.getElementById("connectedAddress").innerHTML = connectedAccount.substring(0, 6).concat("...").concat(connectedAccount.substr(-4));
 }
 
 async function getTwitterID(identifier) {
@@ -431,25 +434,34 @@ async function getTwitterID(identifier) {
 }
 
 async function translateTwitter() {
-    let twitterName = document.getElementById("twitterHandle").value;
-    let userId = await getTwitterID(twitterName);
-    return await convert(userId, 'bytes32');
+    let twitterHandle = document.getElementById("twitterHandle").value;
+    document.getElementById("twitterName").innerHTML = twitterHandle;
+    let userIdString = await getTwitterID(twitterHandle);
+    userId =  await convert(userIdString, 'bytes32');
+    // hide input and show tweet content
+    userHash = web3.utils.keccak256(userId, connectedAccount);
+    userTweet = "I am verifying my Twitter account: ", userHash
+    document.getElementById("tweetContentDiv").style.display = "";
+    document.getElementById("tweetContent").innerHTML = userTweet;
+    document.getElementById("twitterDiv").style.display = "none";
 }
 
-const convert = (value, toType) => {
+async function convert(value, toType) {
     let output;
     if (toType === 'bytes32') {
         output =  web3.utils.asciiToHex(value);
     }
     if (toType === 'string') {
-      output = web3.utils.hexToAscii(value);
+        output = web3.utils.hexToAscii(value);
     }
     return output;
-  };
+}
 
-async function makeAssertion(userId) {
+
+async function makeAssertion() {
     dataId = userId;
-    data = "User with userId " + userId + " has tweeted a verification tweet here: " + tweetURL + ", claiming they own address " + connectedAccount + "."
+    let tweetURL = document.getElementById("tweetUrl").value;
+    data = "User with userId " + userId + " has tweeted a verification tweet here: " + tweetURL + ", claiming they own address " + connectedAccount + ". userId converted through `web3.utils.asciiToHex({stringUserId})`. Hash verification through web3.utils.keccak256({userId}, {address})."
     // set it automatically on contract level?
     asserter = connectedAccount;
 
@@ -458,10 +470,28 @@ async function makeAssertion(userId) {
 
 }
 
+function toggleTwitterDiv() {
+    var twitterDiv = document.getElementById("twitterDiv");
+    if (twitterDiv.style.display === "none") {
+      twitterDiv.style.display = "block";
+    } else {
+      twitterDiv.style.display = "none";
+    }
+  }
+
+async function copyTweet() {
+    let content = document.getElementById("tweetContent").innerHTML;
+    await navigator.clipboard.writeText(content);
+    document.getElementById("tooltip").style.display = "block";
+    setTimeout(function () {
+        tooltip.style.display = "none";
+    }, 1000);
+}
+
 // twitter name
-// 1. get id
-// 2. translate id to bytes32
-// 3. create <h> = hash(userId + address)
+// 1. get id - done
+// 2. translate id to bytes32 - done
+// 3. create <h> = hash(userId + address) 
 // 4. create template tweet content including <h>
 // 5. display 4.
 // 6. call assert -> makeAssertion
